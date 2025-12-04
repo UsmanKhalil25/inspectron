93452daa122d345ba66ec3dffb2e860be7339c9b
UsmanKhalil25 <usmankhalil8011@gmail.com>
2025-12-05 01:05:42 +0500

    feat: update package dependencies and add build script for all workspaces

--- api/src/screencast/screencast.gateway.ts
+++ api/src/screencast/screencast.gateway.ts
@@ -9,7 +9,7 @@ import {
 } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 import { Logger } from "@nestjs/common";
-import { ScreencastManager } from "core/dist/crawler/screencast-manager";
+import { ScreencastManager } from "core/dist/crawler";
 import {
   WSMessageType,
   type StartScreencastMessage,
