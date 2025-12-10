1024c6d0a8ca69111f52a02a0cd5d3688d9bd55c
UsmanKhalil25 <usmankhalil8011@gmail.com>
2025-12-11 02:11:39 +0500

    refactor: labeling service

--- core/src/services/labeling-service.ts
+++ core/src/services/labeling-service.ts
@@ -1,11 +1,21 @@
 import { Page } from "playwright";
-
 import { PageElement } from "../types";
 
 export class LabelingService {
   static async labelElements(page: Page, elements: PageElement[]) {
     await page.evaluate((items) => {
+      function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+
       items.forEach((el) => {
+        const borderColor = getRandomColor();
+
         const div = document.createElement("div");
         div.style.position = "absolute";
         div.style.left = `${el.boundingBox.x + window.scrollX}px`;
@@ -13,21 +23,23 @@ export class LabelingService {
         div.style.width = `${el.boundingBox.width}px`;
         div.style.height = `${el.boundingBox.height}px`;
 
-        div.style.border = "2px dashed red";
-        div.style.zIndex = "999999";
+        div.style.outline = `2px dashed ${borderColor}`;
         div.style.pointerEvents = "none";
+        div.style.zIndex = "2147483647";
+        div.style.boxSizing = "border-box";
 
         const label = document.createElement("div");
         label.innerText = `${el.id}`;
         label.style.position = "absolute";
-        label.style.top = "-18px";
+        label.style.top = "-19px";
         label.style.left = "0";
         label.style.padding = "2px 4px";
-        label.style.background = "red";
+        label.style.background = borderColor;
         label.style.color = "white";
-        label.style.fontSize = "16px";
+        label.style.fontSize = "12px";
         label.style.fontWeight = "bold";
         label.style.fontFamily = "monospace";
+        label.style.borderRadius = "2px";
 
         div.appendChild(label);
         document.body.appendChild(div);
