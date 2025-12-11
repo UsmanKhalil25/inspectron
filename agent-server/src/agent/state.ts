cb319f5e7d03753c3a7ae169cab39658922da890
UsmanKhalil25 <usmankhalil8011@gmail.com>
2025-12-11 20:22:48 +0500

    do a bunch of stuff

---
+++ agent-server/src/agent/state.ts
@@ -0,0 +1,11 @@
+import * as z from "zod";
+import { MessagesZodMeta } from "@langchain/langgraph";
+import { registry } from "@langchain/langgraph/zod";
+import { type BaseMessage } from "@langchain/core/messages";
+
+export const MessagesState = z.object({
+  messages: z
+    .array(z.custom<BaseMessage>())
+    .register(registry, MessagesZodMeta),
+  llmCalls: z.number().optional(),
+});
